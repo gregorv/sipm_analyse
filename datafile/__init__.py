3675c573dbb0915db2481db0971efecf7396a9d5
Gregor Vollmer <gregor@celement.de>
2013-12-04 12:05:46 +0100

    Added raw CSV importer

--- datafile/__init__.py
+++ datafile/__init__.py
@@ -5,3 +5,11 @@ if major_version == 2:
     from py2 import *
 elif major_version == 3:
     from .py3 import *
+
+
+def import_raw_csv(filename):
+    with open(filename) as f:
+        for line in f:
+            if line.startswith('#') or not line:
+                continue
+            yield tuple(map(float, map(str.strip, line.split())))
