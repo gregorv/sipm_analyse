9628f653169af3f16f1455b900ad2be95a70eecb
vollmer-g <vollmer-g@fd7b642b-f7a7-476b-83dd-42b6f92cd3b3>
2014-06-17 13:18:53 +0000

    Binary format user headers and YAML binary support.
    User headers in the get_data binary format are now
    interpreted correctly and can be retrieved with
    a convenience method.
    
    YAML binaries have a single YAML document as
    header, followed by raw binary data, similar
    to "the binary" format. Note that YAML binaries
    are not supported by PyPy.
    
    So, duh, it was not a smart idea to implement it in
    the first place.
    
    And so the code is not tested.
    
    Have fun with it =)
    
    git-svn-id: https://devel-ik.fzk.de/svn/lhc2cr/sipm/projects/Gregor@1192 fd7b642b-f7a7-476b-83dd-42b6f92cd3b3

--- datafile/py2.py
+++ datafile/py2.py
@@ -3,7 +3,10 @@ import sys
 import struct
 import numpy as np
 import os
-
+import re
+import platform
+if platform.python_implementation() != "PyPy":
+    import yaml
 
 def import_frame(filename):
     with open(filename, "rb") as f:
@@ -52,12 +55,18 @@ def import_frame_txt(f):
 def import_dta(filename, dataset=None):
     with open(filename, "rb") as f:
         header = f.read(20)
-        (version, samples_per_frame, num_frames, compressed) = \
-            struct.unpack("xxxxxBHI?xxxxxxx", header)
+        if len(header) < 20:
+            raise Exception("Data file broken")
+        (m0, m1, m2, m3, m4, version, samples_per_frame,
+         num_frames, flags, data_offset) = \
+            struct.unpack("cccccBHIBxxxHxx", header)
+        if "".join((m0, m1, m2, m3, m4)) != "#DTA\n":
+            raise Exception("Invalid magic token")
         if version != 1:
             raise Exception("Unsupported file format version")
-        if compressed:
+        if flags & 1:
             raise Exception("Compression of input files not supported")
+        f.seek(data_offset, 1)
         frame_idx = 0
         if dataset is None:
             for i in xrange(num_frames):
@@ -79,19 +88,111 @@ def import_dta(filename, dataset=None):
                 yield np.array(time), np.array(signal), i
 
 
+def import_yaml(filename, dataset=None):
+    with open(filename, "rb") as f:
+        header = f.read(3)
+        if header != "---":
+            raise Exception("No YAML data file")
+        while data[-4:] != "\n...":
+            header += f.read(1)
+        print "YAML header", header
+        header = yaml.safe_load(header)
+        data_offset = f.tell()
+        f.seek(0, 1)
+        data_size = f.tell() - data_offset
+        num_frames = data_size / (2*header["samples_per_frame"]*4)
+        print "Num frames", num_frames
+        if dataset is None:
+            for i in xrange(num_frames):
+                time = f.read(samples_per_frame*4)
+                time = struct.unpack("f"*samples_per_frame, time)
+                signal = f.read(samples_per_frame*4)
+                signal = struct.unpack("f"*samples_per_frame, signal)
+                yield np.array(time), np.array(signal), i
+        else:
+            for i in sorted(dataset):
+                data_pos = 20+samples_per_frame*8*i
+                delta_seek = data_pos-f.tell()
+                if delta_seek > 0:
+                    f.seek(delta_seek, 1)
+                time = f.read(samples_per_frame*4)
+                time = struct.unpack("f"*samples_per_frame, time)
+                signal = f.read(samples_per_frame*4)
+                signal = struct.unpack("f"*samples_per_frame, signal)
+                yield np.array(time), np.array(signal), i  
+
+
+def get_yaml_header(filename):
+    with open(filename, "rb") as f:
+        header = f.read(3)
+        if header != "---":
+            raise Exception("No YAML data file")
+        while data[-4:] != "\n...":
+            header += f.read(1)
+        print "YAML header", header
+        header = yaml.safe_load(header)
+        data_offset = f.tell()
+        f.seek(0, 1)
+        data_size = f.tell() - data_offset
+        num_frames = data_size / (2*header["samples_per_frame"]*4)
+        header["num_frames"] = num_frames
+        return header
+
+
 def get_dta_info(filename):
     with open(filename, "rb") as f:
         header = f.read(20)
-        (m0, m1, m2, m3, m4, version, samples_per_frame, num_frames, compressed) = \
-            struct.unpack("cccccBHI?xxxxxxx", header)
+        if len(header) < 20:
+            raise Exception("Data file broken")
+        fmt = "cccccBHIBxxxHxx"
+#        print "Struct size", struct.calcsize(fmt)
+#        print "BLA", repr(header), len(header)
+        (m0, m1, m2, m3, m4, version,
+         samples_per_frame, num_frames, flags, data_offset) = \
+            struct.unpack(fmt, header)
         if "".join((m0, m1, m2, m3, m4)) != "#DTA\n":
             return None
         else:
             return {"version": version,
                     "samples_per_frame": samples_per_frame,
                     "num_frames": num_frames,
-                    "compressed": compressed
+                    "compressed": (flags & 1) != 0,
+                    "auto_trigger": (flags & 2) != 0,
+                    "data_offset": data_offset,
                     }
+
+
+def get_dta_userheader(filename):
+    with open(filename, "rb") as f:
+        header = f.read(20)
+        (m0, m1, m2, m3, m4, version, data_offset) = \
+            struct.unpack("cccccBxxxxxxxxxxHxx", header)
+        if "".join((m0, m1, m2, m3, m4)) != "#DTA\n":
+            raise Exception("Not dta file!")
+        header_dict = {}
+        for line in filter(lambda x: x, f.read(data_offset).split("\n")):
+            key, value = map(str.strip, line.split("="))
+            if key.startswith("# "):
+                key = key[2:]
+            if key.endswith("_f"):
+                match = re.search("^(-?[0-9]*\.?[0-9]*)(.?)", value)
+                try:
+                    prefix_value = {"T": 1e12,
+                                    "G": 1e9,
+                                    "M": 1e6,
+                                    "k": 1e3,
+                                    "d": 1e-1,
+                                    "c": 1e-2,
+                                    "m": 1e-3,
+                                    "u": 1e-6,
+                                    "n": 1e-9,
+                                    "p": 1e-12,
+                                    "f": 1e-15}[match.group(2)]
+                except KeyError:
+                    prefix_value = 1.0
+                value = float(match.group(1))*prefix_value
+            header_dict[key] = value
+        return header_dict
 
 
 def get_all_data(arg_files, dataset=None):
@@ -114,21 +215,33 @@ def get_all_data(arg_files, dataset=None):
         num_frames = dta_info["num_frames"] if dataset is None else len(dataset)
         for time, signal, frame_idx in import_dta(arg_files[0], dataset):
             yield num_frames, frame_idx, time, signal
-    else:
-        for path in arg_files:
-            if os.path.isdir(path):
-                directories.append(path)
-            elif path.endswith(".dat") or path.endswith(".csv"):
-                files_to_process.add(path)
-        discover_files()
-        while(len(files_to_process)):
-            filename = files_to_process.pop()
-            files_processed.add(filename)
-            if len(files_to_process) == 0:
-                discover_files()
-            num_frames = len(files_processed) + len(files_to_process)
-            frame_idx = len(files_processed)
-            #print filename
-            time, signal = import_frame(filename)
-            yield (num_frames, frame_idx,
-                   time, signal)
+        return
+    print "STEP"
+    if len(arg_files) == 1:
+        print "Try YAML"
+        try:
+            header = get_yaml_header(arg_files[0])
+            num_frames = header["num_frames"]
+            for time, signal, frame_idx in import_yaml(arg_files[0], dataset):
+                yield num_frames, frame_idx, time, signal
+            return
+        except Exception, e:
+            print e
+    
+    for path in arg_files:
+        if os.path.isdir(path):
+            directories.append(path)
+        else:
+            files_to_process.add(path)
+    discover_files()
+    while(len(files_to_process)):
+        filename = files_to_process.pop()
+        files_processed.add(filename)
+        if len(files_to_process) == 0:
+            discover_files()
+        num_frames = len(files_processed) + len(files_to_process)
+        frame_idx = len(files_processed)
+        #print filename
+        time, signal = import_frame(filename)
+        yield (num_frames, frame_idx,
+               time, signal)
